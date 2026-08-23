42455f1f0745b3e3efb97ed0cf2d0e1ce94c78bc
Paul Louth <p.louth@gmail.com>
2026-08-23 22:29:33 +0100

    Move `TryGetValue` and `CopyTo` implementations from `Iterator.Extensions.Ref` to `Iterator` for simplified structure and centralized logic.

--- src/Iterator2/Iterator.Extensions.Ref.cs
+++ src/Iterator2/Iterator.Extensions.Ref.cs
@@ -1,7 +1,4 @@
 using System.Runtime.CompilerServices;
-using IteratorPrototype.Internal;
-using IteratorPrototype.Internal.Collections;
-using IteratorPrototype.Internal.Source.Factories;
 using IteratorPrototype.Internal.Sources;
 
 namespace IteratorPrototype;
@@ -10,52 +7,6 @@ public static partial class IteratorExtensions2
 {
     extension<A>(ref Iterator2<A> ta)
     {
-        [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
-        public unsafe bool TryGetValue(out A head, out Iterator2<A> tail)
-        {
-            // Copy
-            // Consider better ways, but remember, `ta` might also be `tail`, which means doing `tail = default` to
-            // initialise it will overwrite `ta`.
-            tail = ta;
-
-            ref var source = ref Unsafe.AsRef(in tail.source);
-            ref var ops    = ref Unsafe.AsRef(in tail.ops);
-            ref var objs   = ref Unsafe.AsRef(in tail.objs);
-            ref var values = ref Unsafe.AsRef(in tail.values);
-            var     frame  = new StackFrame(ref source, ref objs, ref values);
-
-            while (source is not null)
-            {
-                if(!source.Run(ref frame)) continue;
-                
-                var hasValue = true;
-                while (ops.NextPC(out var op))
-                {
-                    if (op.Run(ref frame)) continue;
-                    hasValue = false;
-                    break;
-                }
-                ops.ResetPC();
-                    
-                if(hasValue)
-                {
-                    ValueStack<A>.Pop(ref frame, out head);
-                    return true;
-                }
-            }
-            head = default!;
-            return false;
-        }
-        
-        [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
-        public void CopyTo(ref Iterator2<A> other)
-        {
-            other.SetSource(in ta.source);
-            ta.ops.CopyTo(ref Unsafe.AsRef(in other.ops));
-            ta.objs.CopyTo(ref Unsafe.AsRef(in other.objs));
-            ta.values.CopyTo(ref Unsafe.AsRef(in other.values));
-        }
-        
         [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
         internal void SetSource(in IteratorSource? source)
         {
--- src/Iterator2/Iterator.cs
+++ src/Iterator2/Iterator.cs
@@ -1,5 +1,7 @@
 using System.Runtime.CompilerServices;
+using IteratorPrototype.Internal;
 using IteratorPrototype.Internal.Collections;
+using IteratorPrototype.Internal.Source.Factories;
 using IteratorPrototype.Internal.Sources;
 
 namespace IteratorPrototype;
@@ -22,4 +24,50 @@ public readonly struct Iterator2<A>
     internal readonly OpFrame ops;
     internal readonly ObjStack objs;
     internal readonly ByteStack values;
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+    public unsafe bool TryGetValue(out A head, out Iterator2<A> tail)
+    {
+        // Copy
+        // Consider better ways, but remember, `ta` might also be `tail`, which means doing `tail = default` to
+        // initialise it will overwrite `ta`.
+        tail = this;
+
+        ref var source = ref Unsafe.AsRef(in tail.source);
+        ref var ops    = ref Unsafe.AsRef(in tail.ops);
+        ref var objs   = ref Unsafe.AsRef(in tail.objs);
+        ref var values = ref Unsafe.AsRef(in tail.values);
+        var     frame  = new StackFrame(ref source, ref objs, ref values);
+
+        while (source is not null)
+        {
+            if(!source.Run(ref frame)) continue;
+                
+            var hasValue = true;
+            while (ops.NextPC(out var op))
+            {
+                if (op.Run(ref frame)) continue;
+                hasValue = false;
+                break;
+            }
+            ops.ResetPC();
+                    
+            if(hasValue)
+            {
+                ValueStack<A>.Pop(ref frame, out head);
+                return true;
+            }
+        }
+        head = default!;
+        return false;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+    public void CopyTo(ref Iterator2<A> other)
+    {
+        other.SetSource(in source);
+        ops.CopyTo(ref Unsafe.AsRef(in other.ops));
+        objs.CopyTo(ref Unsafe.AsRef(in other.objs));
+        values.CopyTo(ref Unsafe.AsRef(in other.values));
+    }
 }
